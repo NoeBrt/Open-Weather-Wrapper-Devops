9ff377b6df50f4890a236e17b944b7f0d1a5d5fd
noe <noe.breton@etu.univ-tours.fr>
2024-03-28 16:37:45 +0100

    update docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Step 1: Specify the base image
+FROM node:20.11-slim
+
+# Step 2: Set the working directory in the container
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+# Step 4: Install dependencies
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+# Step 7: Define the command to run your app using CMD which defines your runtime
+CMD [ "node", "index.js" ]
--- OpenWeatherWrapper.js
+++ OpenWeatherWrapper.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const axios = require('axios');
 
 const getWeatherData = async () => {
-  const apiKey = process.env.OPENWEATHER_API_KEY;
-  const latitude = process.env.LATITUDE;
-  const longitude = process.env.LONGITUDE;
+  const apiKey = process.env.API_KEY;
+  const latitude = process.env.LAT;
+  const longitude = process.env.LON;
   const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
   const url = `${baseUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
