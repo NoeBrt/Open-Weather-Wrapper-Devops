c193d30ef3c92b2dacabbfe45c3bd9b7db55c6af
noebrt <noe.breton@protonmail.com>
2024-04-13 15:32:15 +0200

    refactor jsclass

--- OpenWeatherWrapper.js
+++ OpenWeatherWrapper.js
@@ -2,13 +2,17 @@
 require('dotenv').config();
 const axios = require('axios');
 
-async function getWeatherData(lat, lon) {
-  const apiKey = process.env.API_KEY;
+async function getWeatherData(lat, lon,apiKey, units) {
   const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-  const url = `${baseUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(baseUrl, {
+      params: {
+          lat,
+          lon,
+          appid: apiKey,
+          units
+      }
+  });
     return response.data;
   } catch (error) {
     console.error('Error fetching weather data:', error);
--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 const express = require('express');
 const getWeatherData = require('./OpenWeatherWrapper'); // Import the weather data fetching function
 const app = express();
+const apiKey=process.env.API_KEY;
+const units="metter";
 const port = process.env.PORT || 8080;
 
-app.get('/weather', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const { lat, lon } = req.query;
     if (!lat || !lon) {
       return res.status(400).json({ error: 'Latitude and longitude are required' });
     }
-    const data = await getWeatherData(lat, lon);
+    const data = await getWeatherData(lat, lon,apiKey,units);
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
